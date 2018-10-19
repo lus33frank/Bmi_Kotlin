efef73a1cda1820ad64d902c5b1364d1c1b5dffe
lus33frank <lus33frank@gmail.com>
2018-10-19 21:30:38 +0800

    專案練習初始化版本。

---
+++ app/src/androidTest/java/com/frankchang/bmi_kotlin/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.frankchang.bmi_kotlin
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.frankchang.bmi_kotlin", appContext.packageName)
+    }
+}
